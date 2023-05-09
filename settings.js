e5113b82a82cb36e13e3e012821e0e4f5965ff71
Sdu24 <132999762+Sdu24@users.noreply.github.com>
2023-05-09 15:46:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,12 +7,12 @@ const chalk = require('chalk')
 */
 
 //Owner Number & Name 
-global.owner = ['94767438882'] //ur owner number
-global.ownername = "Janith sadanuwan" //ur owner name
+global.owner = ['94789654448'] //ur owner number
+global.ownername = "sadun" //ur owner name
 
 //Bot Name 
-global.botname = 'ǫᴜᴇᴇɴ ɴɪʟᴜ' //ur bot name
-global.wm = "ǫᴜᴇᴇɴ ɴɪʟᴜ" //ur watermark
+global.botname = 'winkly' //ur bot name
+global.wm = "ǫᴜᴇᴇɴ winkly" //ur watermark
 
 global.keyopenai = "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",
 //React Messages 
@@ -33,7 +33,7 @@ global.READ_MASSAGE = true
 global.VOICE_REPLY = true
 
 // Do you want show time on your bio use "true" want desable use "false"
-global.AUTO_BIO = false
+global.AUTO_BIO = off
 
 // Inbox massage block PM block
 global.INBOX_BLOCK = false
